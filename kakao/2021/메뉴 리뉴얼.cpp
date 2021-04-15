e19aeb375369cbdac896c4069d0c0c1af6300c24
chocoyam <33726146+chocoyam@users.noreply.github.com>
2021-04-16 00:24:57 +0900

    Update 메뉴 리뉴얼.cpp

--- kakao/2021/메뉴 리뉴얼.cpp
+++ kakao/2021/메뉴 리뉴얼.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<string> Orders = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
 vector<int> Course = {2, 3, 5};
 
-map<string, int> ordersByCourse[Course.size()];
+map<string, int> ordersByCourse[3];
 
 string sortString(string str)
 {
@@ -14,10 +14,20 @@ string sortString(string str)
     return string(chVector.begin(), chVector.end());
 }
 
-vector<string> splitStrByLen(string str, int len)
+void splitStrByLen(string str, int courseIdx)
 {
-    vector<string> returnVec;
+    int len = Course[courseIdx];
+    
+    if (str.size() < len) return;
+    
     string sortedStr = sortString(str);
+    
+    if (len == str.size()) {
+        if (ordersByCourse[courseIdx].find(str) == ordersByCourse[courseIdx].end()) ordersByCourse[courseIdx][str] = 1;
+        else ordersByCourse[courseIdx][str] += 1;
+        return;
+    }
+
     vector<char> charVec(str.begin(), str.end());
     vector<bool> combVec(charVec.size(), true);
     for (int i = 0; i < charVec.size() - len; i++) combVec[i] = false;
@@ -28,7 +38,8 @@ vector<string> splitStrByLen(string str, int len)
         for (int i = 0; i < charVec.size(); i++) 
             if (combVec[i]) menu += charVec[i];
         
-        returnVec.push_back(menu);
+        if (ordersByCourse[courseIdx].find(menu) == ordersByCourse[courseIdx].end()) ordersByCourse[courseIdx][menu] = 1;
+        else ordersByCourse[courseIdx][menu] += 1;
     } while(next_permutation(combVec.begin(), combVec.end()));
 }
 
@@ -40,6 +51,29 @@ bool comp(pair<string, int> lhs, pair<string, int> rhs)
 
 int main()
 {
+    for (string order : Orders) {
+        for (int i = 0; i < Course.size(); i++) {
+            splitStrByLen(order, i);
+        }
+    }
+    
+    vector<string> answer;
+    for (auto map : ordersByCourse) {
+        int max = 0;
+        vector<string> tempAnswer;
+        for (auto data : map) {
+            if (data.second > 1 && data.second >= max) {
+                if (data.second > max) tempAnswer.clear();
+                max = data.second;
+                tempAnswer.push_back(data.first);
+            }
+        }
+        answer.insert(answer.end(), tempAnswer.begin(), tempAnswer.end());
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    for (string str : answer) cout << str << endl;
 
     return 0;
 }
